a76e59d0403fba7faae73f606a601506de336100
aiken516 <jimmy000516@naver.com>
2020-04-29 10:28:58 +0900

    dd

--- 201911249-MidTerm/src/main/java/_MidTerm/_MidTerm/main.java
+++ 201911249-MidTerm/src/main/java/_MidTerm/_MidTerm/main.java
@@ -1,6 +1,5 @@
 package _MidTerm._MidTerm;
 
-
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,11 +10,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import midterm.problem2.RegularExpression;
+
 public class main {
 	public static void main(String args[]) throws ParseException {
 		Scanner console = new Scanner(System.in);
 		JSONParser parser = new JSONParser();
-
+		RegularExpression re = new RegularExpression();
+		
 		try {
 			
 			Object obj=parser.parse(new FileReader("midterm.json"));
@@ -26,12 +28,16 @@ public class main {
 			System.out.println("Enter the keyword you are looking for: ");
 			String str = console.next();
 
+			String[] sentence = str.split("|");
+			String s1 = sentence[0];
+			String s2 = sentence[1];
 			
 		     for(int i = 0; i<infoArray.size(); i++) {
 		      	JSONObject itemObject = (JSONObject) infoArray.get(i);
 		      	
 		      String item = (String) itemObject.get("item");
-		      if (item.contains(str))
+		         
+		      if (re.check(s1, s2))
 		      	System.out.println("item"+ i + ": " + item);
 		      }
 		  }
